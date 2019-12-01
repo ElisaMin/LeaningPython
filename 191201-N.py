1bc87f38e33dbbdb5b494b8827485ec73bb834ec
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-12-01 23:39:18 +0800

    Create 191201-N.py

---
+++ 191201-N.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('just for test')
+window.geometry('500x800')
+
+lable1 = tk.Label(window, text="lgup_cmd gui工具", width=15, height="3")
+lable1.pack()
+window.mainloop()
+'''
+
+如果 不存在 %program files%lge/lgup/lgup_cmd.exe :
+    print("找不到文件")
+    print("退出或者留下")
+    askopenfile 选择文件
+
+帮助(LGE.FUN)
+lgup_cmd助手
+com:
+dll:
+kdz:
+【刷入】
+【二维码】
+【责任声明】
+'''
