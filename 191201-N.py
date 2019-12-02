ef3e078351f17579e3c57d63833a7f10afe68465
Heizi <35397814+ElisaMin@users.noreply.github.com>
2019-12-02 08:58:00 +0800

    Update 191201-N.py

--- 191201-N.py
+++ 191201-N.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+import os 
+
+def shell (cmd):
+    return os.popen(cmd).read()
 
 window = tk.Tk()
 window.title('just for test')
@@ -8,11 +12,11 @@ lable1 = tk.Label(window, text="lgup_cmd gui工具", width=15, height="3")
 lable1.pack()
 window.mainloop()
 '''
-
-如果 不存在 %program files%lge/lgup/lgup_cmd.exe :
+if str(shell(if exist anyway (echo 0) else (echo 1)))==1 :
     print("找不到文件")
     print("退出或者留下")
     askopenfile 选择文件
+
 
 帮助(LGE.FUN)
 lgup_cmd助手
